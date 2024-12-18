6e806724b7d731e973ef8d7c98611a7751b4be11
hsp <Hiran Patel>
2024-12-18 15:23:53 -0500

    feat: update dependencies and build script for improved cross-compilation support

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,13 +9,18 @@ if ! command -v cross &>/dev/null; then
   cargo install cross
 fi
 
+# Build for Linux with static linking
+#RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
+
 # Build for macOS
 echo "Building for macOS..."
 cross build --target x86_64-apple-darwin --release
 
 # Build for Linux
 echo "Building for Linux..."
-cross build --target x86_64-unknown-linux-gnu --release
+#cross build --target x86_64-unknown-linux-gnu --release
+RUSTFLAGS='-C target-feature=+crt-static' cross build --release --target x86_64-unknown-linux-gnu
+# OPENSSL_STATIC=1 RUSTFLAGS='-C target-feature=+crt-static' cargo build --target x86_64-unknown-linux-gnu --release
 
 # Build for Windows
 echo "Building for Windows..."
